4a845c01600dd286540b23f6439546aefb7186a0
jakob <s37136912student.rmit.edu.au>
2019-09-17 01:02:30 +1000

    getStock

--- stock-trading-core/src/main/java/edu/rmit/sef/stock/command/AddStockCmd.java
+++ stock-trading-core/src/main/java/edu/rmit/sef/stock/command/AddStockCmd.java
@@ -10,27 +10,34 @@ public class AddStockCmd extends Command<CreateEntityResp> {
     private double price;
 
     public String getSymbol() {
+
         return symbol;
     }
 
     public void setSymbol(String symbol) {
+
         this.symbol = symbol;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public double getPrice() {
+
         return price;
     }
 
     public void setPrice(double price) {
+
         this.price = price;
     }
+
 
 }
