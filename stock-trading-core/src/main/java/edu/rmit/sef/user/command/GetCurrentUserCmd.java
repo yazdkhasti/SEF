411354016e19fb69eb8982d9d5b5ee2f1ead28a4
Payam Yazdkhasti <yazdkhasti@hotmail.com>
2019-09-25 22:38:54 +1000

    Test issues

--- stock-trading-core/src/main/java/edu/rmit/sef/user/command/GetCurrentUserCmd.java
+++ stock-trading-core/src/main/java/edu/rmit/sef/user/command/GetCurrentUserCmd.java
@@ -1,7 +1,10 @@
 package edu.rmit.sef.user.command;
 
 import edu.rmit.command.core.Command;
+import edu.rmit.command.security.CommandAuthority;
+import edu.rmit.sef.core.security.Authority;
 
+@CommandAuthority(Authority.USER)
 public class GetCurrentUserCmd extends Command<GetCurrentUserResp> {
 
 }
--- stock-trading-core/src/main/java/edu/rmit/sef/user/command/LogoutCmd.java
+++ stock-trading-core/src/main/java/edu/rmit/sef/user/command/LogoutCmd.java
@@ -2,6 +2,9 @@ package edu.rmit.sef.user.command;
 
 import edu.rmit.command.core.Command;
 import edu.rmit.command.core.NullResp;
+import edu.rmit.command.security.CommandAuthority;
+import edu.rmit.sef.core.security.Authority;
 
+@CommandAuthority(Authority.USER)
 public class LogoutCmd extends Command<NullResp> {
 }
